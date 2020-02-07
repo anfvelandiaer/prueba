eaf8a1e22b2dd74285eeaea24f3ac970b25de6ae
anfvelandiaer <anfvelandiaer@unal.edu.co>
2020-02-07 08:43:17 -0500

    Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {//Firebase
     apiKey: "AIzaSyCngB9SFNFHvErLYJcd3Nyt61eqpiPK1As",
     authDomain: "prueba-a3708.firebaseapp.com",
