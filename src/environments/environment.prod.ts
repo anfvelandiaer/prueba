25ec880587f3c1e0015aa0fb4f8353e4bf4a6c5b
anfvelandiaer <anfvelandiaer@unal.edu.co>
2020-02-07 09:03:47 -0500

    c

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {//Firebase
+    apiKey: "AIzaSyCngB9SFNFHvErLYJcd3Nyt61eqpiPK1As",
+    authDomain: "prueba-a3708.firebaseapp.com",
+    databaseURL: "https://prueba-a3708.firebaseio.com",
+    projectId: "prueba-a3708",
+    storageBucket: "prueba-a3708.appspot.com",
+    messagingSenderId: "410595142649",
+    appId: "1:410595142649:web:e88eccfdf44b82f280eb28"
+  }
 };
