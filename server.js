4ce7755d12d5016c39f1217189ed322c47dcc4c2
anfvelandiaer <anfvelandiaer@unal.edu.co>
2020-02-07 09:27:08 -0500

    d

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.use(express.static(__dirname+'/dist/prueba'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/prueba/index.html'));
 });
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'src', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
